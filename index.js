152b55edf9079e72d7d9c79c3e437bf0e5389c86
Md Emon Hossain <96044760+S1Emon@users.noreply.github.com>
2022-05-23 21:15:59 +0600

    single data added

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const app = express();
 const cors = require('cors')
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
-// middle ware
+// middle-ware
 app.use(cors());
 app.use(express.json());
-
-// BlxTvbZ7ID28SRO7
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.z1z7z.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -27,15 +24,33 @@ const run = async () => {
             const result = await productCollection.find(query).toArray()
             res.send(result);
         })
+        // single data load system
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const product = await productCollection.findOne(query)
+            res.send(product);
+        })
+
+
 
         // Create data
-
+        app.post('/product', async (req, res) => {
+            const newProduct = req.body;
+            const result = await productCollection.insertOne(newProduct);
+            res.send(result);
+        })
 
         //Update Data
 
 
         //Delete Data
-
+        app.delete('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const result = await productCollection.deleteOne(filter);
+            res.send(result);
+        })
 
 
     }
@@ -45,15 +60,6 @@ const run = async () => {
 
 }
 run().catch(console.dir);
-
-
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
-
-
 
 
 app.listen(port, () => {
